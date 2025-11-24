e9ed685f81e8c0c6b837607c804ffb245d5a76ef
PabloPlu <874167@unizar.es>
2025-11-24 17:52:54 +0100

    cambios en el aleatorio del nrf

---
+++ nrf/src_nrf/hal_random_nrf.c
@@ -0,0 +1,43 @@
+/* *****************************************************************************
+ * P.H.2025: Implementación de números aleatorios para el NRF
+ * Este fichero utiliza la API ofrecida por el hardware dedicado del procesador
+ */
+
+#include "random.h"
+
+void random_iniciar(uint64_t seed)
+{
+		// no es necesaria la seed en nrf
+    (void)seed;
+    NRF_RNG->CONFIG = RNG_CONFIG_DERCEN_Enabled << RNG_CONFIG_DERCEN_Pos;
+    NRF_RNG->EVENTS_VALRDY = 0;
+    NRF_RNG->TASKS_START = 1;
+}
+
+static uint8_t random8(void)
+{
+    // espera a que haya un byte listo
+    while (NRF_RNG->EVENTS_VALRDY == 0);
+		// limpia evento
+    NRF_RNG->EVENTS_VALRDY = 0;    
+    return NRF_RNG->VALUE;          
+}
+
+uint32_t random_value(uint32_t min, uint32_t max)
+{
+    // intercambiamos para q el rango no de problemas
+    if (min > max) {
+        min ^= max;
+        max ^= min;
+        min ^= max;
+    }
+
+    // construimos número de 32 bits
+    uint32_t r =
+        ((uint32_t)random8() << 24) |
+        ((uint32_t)random8() << 16) |
+        ((uint32_t)random8() << 8)  |
+        ((uint32_t)random8());
+
+    return min + (r % (max - min + 1));
+}
