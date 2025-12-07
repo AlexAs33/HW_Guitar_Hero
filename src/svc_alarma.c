6853eba5fdb9bfb6ea539ad667906c600a8bdfd4
PabloPlu <874167@unizar.es>
2025-12-07 18:26:19 +0100

    alarmas bien

--- src/svc_alarma.c
+++ src/svc_alarma.c
@@ -18,22 +18,18 @@ static svc_alarma_t alarmas[svc_ALARMAS_MAX];
 static MONITOR_id_t monitor_overflow;
 static void (*f_cb)(EVENTO_T, uint32_t);
 EVENTO_T evento;
-//static Tiempo_ms_t periodo_evento_ms;
 
 /* Inicialización */
 void svc_alarma_iniciar(MONITOR_id_t M_overflow, void (*cb_a_llamar)(EVENTO_T, uint32_t), EVENTO_T ev_a_notificar)
 {
     monitor_overflow = M_overflow;
     f_cb = cb_a_llamar;
-		evento = ev_a_notificar;
-	
-    //periodo_evento_ms = rt_GE_TIMEOUT_INACTIVIDAD_MS;
+	evento = ev_a_notificar;
 
     for (int i = 0; i < svc_ALARMAS_MAX; i++) alarmas[i].activa = false;
 
     // Cada vez que se llegue al periodo, se encolará ev_T_PERIODICO
-	  drv_tiempo_periodico_ms(svc_ALARMAS_PERIODO_MS, cb_a_llamar, ev_a_notificar);
-    //drv_tiempo_periodico_ms(periodo_evento_ms, cb_a_llamar, ev_a_notificar);
+	drv_tiempo_periodico_ms(svc_ALARMAS_PERIODO_MS, cb_a_llamar, ev_a_notificar);
 
     svc_GE_suscribir(ev_a_notificar, 0, svc_alarma_actualizar);
 }
@@ -47,34 +43,6 @@ uint32_t svc_alarma_codificar(bool periodico, Tiempo_ms_t retardo_ms, uint8_t fl
     return f;
 }
 
-
-// Calculo del minimo comun divisor
-/*
-static Tiempo_ms_t mcd(Tiempo_ms_t a, Tiempo_ms_t b) {
-    while (b != 0) {
-        Tiempo_ms_t tmp = b;
-        b = a % b;
-        a = tmp;
-    }
-    return a;
-}
-
-// Calculo del minimo comun divisor entre el periodo de todas las alarmas
-static Tiempo_ms_t calcular_mcd_global(void)
-{
-    Tiempo_ms_t m = 0;
-    for (int i = 0; i < svc_ALARMAS_MAX; i++) {
-        if (alarmas[i].activa) {
-            if (m == 0)
-                m = alarmas[i].contador_ms;
-            else
-                m = mcd(m, alarmas[i].contador_ms);
-        }
-    }
-    return (m == 0) ? svc_ALARMAS_PERIODO_MS : m;
-}
-*/
-
 /* Activar o reprogramar una alarma */
 void svc_alarma_activar(uint32_t alarma_flags, EVENTO_T ID_EVENTO, uint32_t aux_Data)
 {
@@ -87,7 +55,6 @@ void svc_alarma_activar(uint32_t alarma_flags, EVENTO_T ID_EVENTO, uint32_t aux_
         for (int i = 0; i < svc_ALARMAS_MAX; i++) {
             if (alarmas[i].activa && alarmas[i].evento == ID_EVENTO && alarmas[i].auxData == aux_Data) 
                 alarmas[i].activa = false;
-			}
         }
         return;
     }
@@ -101,10 +68,6 @@ void svc_alarma_activar(uint32_t alarma_flags, EVENTO_T ID_EVENTO, uint32_t aux_
             alarmas[i].contador_ms = retardo_ms;
             alarmas[i].evento = ID_EVENTO;
             alarmas[i].auxData = aux_Data;
-
-            // se actualiza el periodo global del ev_T_PERIODICO
-						//periodo_evento_ms = calcular_mcd_global();
-						//drv_tiempo_periodico_ms(periodo_evento_ms, f_cb, evento);
 					
 #ifdef DEBUG
 //						char buf[64];
@@ -142,8 +105,8 @@ void svc_alarma_actualizar(EVENTO_T evento, uint32_t aux)
                         alarmas[i].contador_ms = alarmas[i].retardo_ms;
                     else
                         alarmas[i].activa = false;
-										}
             }
-				}
+        }
     }
 }
+
